46729c1055910bd542fc90c1a3f709832934d7e2
Baus163 <nguyenthanhdat163190@gmail.com>
2024-11-20 16:28:11 +0700

    Add files via upload

---
+++ Chuong2/src/bai10/chuvi_dientich_hcn.java
@@ -0,0 +1,15 @@
+package bai10;
+import java.util.Scanner;
+public class chuvi_dientich_hcn {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap vao chieu dai hinh chu nhat : ");
+        int cd = sc.nextInt();
+        System.out.println("nhap vao chieu rong hinh chu nhat : ");
+        int cr = sc.nextInt();
+        int chuvi=(cd+cr)*2;
+        int dientich=cd*cr;
+        System.out.println("dien tich hinh chu nhat la : "+dientich);
+        System.out.println("chu vi hinh chu nhat la : "+chuvi);
+    }
+}
